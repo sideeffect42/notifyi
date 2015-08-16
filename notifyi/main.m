cf076e72b67159644d770d36c89e6dcf5a75573d
Takashi Yoshi <takashi@yoshi.email>
2015-08-16 23:22:01 +0200

    Downgrade to support older versions of Mac OS X (10.3) and Xcode (3.1)

--- notifyi/main.m
+++ notifyi/main.m
@@ -2,37 +2,27 @@
 
 #import "AppDelegate.h"
 
+AppDelegate *app = nil;
+
+void SignalHandler(int sig) {
+	fprintf(stderr, "Stopping...\n");
+	[app stop];
+	exit(EXIT_SUCCESS);	
+}
+
 int main(int argc, const char *argv[])
 {
-	@autoreleasepool
-	{
-		AppDelegate *app = [[AppDelegate alloc] init];
-		
-		signal(SIGINT, SIG_IGN);
-		signal(SIGTERM, SIG_IGN);
-		
-		dispatch_source_t _intSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_SIGNAL, SIGINT, 0, dispatch_get_main_queue());
-		dispatch_source_t _termSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_SIGNAL, SIGTERM, 0, dispatch_get_main_queue());
-		
-		dispatch_block_t handler = ^{
-			
-			fprintf(stderr, "Stopping...\n");
-			
-			[app stop];
-			
-			exit(EXIT_SUCCESS);
-		};
-		
-		dispatch_source_set_event_handler(_intSource, handler);
-		dispatch_source_set_event_handler(_termSource, handler);
-		
-		dispatch_resume(_intSource);
-		dispatch_resume(_termSource);
-		
-		[app start];
-		
-		[[NSRunLoop mainRunLoop] run];
-	}
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	app = [[AppDelegate alloc] init];
+	
+	signal(SIGINT, SignalHandler);
+	signal(SIGTERM, SignalHandler);
+	
+	[app start];
+	
+	[[NSRunLoop currentRunLoop] run];
+
+	[pool release];
 	
 	return EXIT_SUCCESS;
 }
